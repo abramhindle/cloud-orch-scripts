4aef7f011421f013e6821e9bc8c829050c87348a
Abram Hindle <abram.hindle@softwareprocess.es>
2014-04-28 23:28:54 -0600

    netjack

--- netjack.sh
+++ netjack.sh
@@ -1,14 +1,6 @@
-jack_netsource -H slave01 -i 1 -o 0 &
-jack_netsource -H slave02 -i 1 -o 0  &
-jack_netsource -H slave03 -i 1 -o 0  &
-jack_netsource -H slave04 -i 1 -o 0 &
-jack_netsource -H slave05 -i 1 -o 0 &
-jack_netsource -H slave06 -i 1 -o 0 &
-jack_netsource -H slave07 -i 1 -o 0 &
-jack_netsource -H slave08 -i 1 -o 0 &
-jack_netsource -H slave09 -i 1 -o 0 &
-jack_netsource -H slave10 -i 1 -o 0 &
-jack_netsource -H slave11 -i 1 -o 0 &
-jack_netsource -H slave12 -i 1 -o 0 &
-jack_netsource -H slave13 -i 1 -o 0 &
-jack_netsource -H slave14 -i 1 -o 0 
+#!/bin/bash
+for file in `cat hosts`
+do
+echo $file
+jack_netsource -H $file -i 1 -o 0 &
+done
