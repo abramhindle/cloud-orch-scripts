c14bddbda88baaa3937930c3c5887188e593a6a2
Abram Hindle <abram.hindle@softwareprocess.es>
2014-04-28 23:31:40 -0600

    netjack

--- start-grains.sh
+++ start-grains.sh
@@ -1,2 +1,2 @@
-cd cloud-orch-scripts
+cd ~/cloud-orch-scripts
 sh slave.sh screen -d -m bash ~/cloud-orch-scripts/grainslavewo.sh
--- start-jackds.sh
+++ start-jackds.sh
@@ -1,2 +1,2 @@
 cd ~/cloud-orch-scripts
-sh slave.sh screen -d -m bash ~/cloud-orch-scripts/grainslavewo.sh
+sh slave.sh screen -d -m bash ~/cloud-orch-scripts/jackd-slave.sh
